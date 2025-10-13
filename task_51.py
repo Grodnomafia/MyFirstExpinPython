3055e20419e9d14233ac157d2cf27dae0ecef1ff
grodnomafia <axevice1234@tut.by>
2025-10-13 19:49:38 +0300

    I solved split and strip

--- task_51.py
+++ task_51.py
@@ -1,1 +1,33 @@
-print('hello world')
+def search_key(text_list , search):
+    if search in text_list:
+        return text_list[search]
+    for i in text_list.values():
+        if isinstance(i, dict):
+            new_search = search_key(i, search)
+            if new_search :
+                break
+    else:
+        new_search = None
+    return new_search
+
+
+site = {
+    'html': {
+        'head': {
+            'title': 'Мой сайт'
+        },
+        'body': {
+            'h2': 'Здесь будет мой заголовок',
+            'div': 'Тут, наверное, какой-то блок',
+            'p': 'А вот здесь новый абзац'
+        }
+    }
+}
+
+
+find_key = input('Искомый ключ: ')
+key = search_key(site, find_key)
+if key:
+    print(key)
+else:
+    print('Ключ не найден')
